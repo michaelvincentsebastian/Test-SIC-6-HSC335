7b015600e37379c9aec5b4c1f250b0f91511129c
Michael Vincent Sebastian Handojo <vincentmichael514@gmail.com>
2025-02-08 11:53:30 +0700

    update printing

--- rectangle_area.py
+++ rectangle_area.py
@@ -6,3 +6,5 @@ length = 5
 width = 10
 area = calculate_area(length, width)
 perimeter = calculate_perimeter(length, width)
+print(f"The area of the rectangle is: {area}") # From Maya's branch
+print(f"The perimeter of the rectangle is: {perimeter}") # From Maya's branch
