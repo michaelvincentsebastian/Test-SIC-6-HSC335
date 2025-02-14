f2f1b6c1811dc23218885434b7297e04967ea62f
Michael Vincent Sebastian Handojo <vincentmichael514@gmail.com>
2025-02-14 10:37:41 +0700

    feature/input

--- rectangle_area.py
+++ rectangle_area.py
@@ -2,8 +2,8 @@ def calculate_area(length, width):
     return length * width 
 def calculate_perimeter(length, width):
     return 2 * (length + width)
-length = 5
-width = 10
+length = int(input("enter the length: "))
+width = int(input("enter the width: "))
 area = calculate_area(length, width)
 perimeter = calculate_perimeter(length, width)
 print(f"The area of the rectangle is: {area}") # From Maya's branch
